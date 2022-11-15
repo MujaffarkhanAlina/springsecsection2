4c60db5fda0630f819406cb56e7ca9c50fdebc83
MujaffarkhanAlina <mujaffar.khan08@gmail.com>
2022-11-15 08:27:53 -0500

    Initial commit

---
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
+	 * Secured /notices - Not Secured /contact - Not Secured
+	 */
+	/*@Override
+	 protected void configure(HttpSecurity http) throws Exception { */
+ 
+		/**
+		 * Default configurations which will secure all the requests
+		 */
+
+		/*
+		 * http .authorizeRequests() .anyRequest().authenticated() .and()
+		 * .formLogin().and() .httpBasic();
+		 */
+
+		/**
+		 * Custom configurations as per our requirement
+		 */
+
+		/*
+		 * http.authorizeRequests().antMatchers("/myAccount").authenticated()
+		 * .antMatchers("/myBalance").authenticated()
+		 * .antMatchers("/myLoans").authenticated()
+		 * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
+		 * .antMatchers("/contact").permitAll() .and() .formLogin().and() .httpBasic();
+		 */
+
+		/**
+		 * Configuration to deny all the requests
+		 */
+
+		/*
+		 * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
+		 * .httpBasic();
+		 */
+
+		/**
+		 * Configuration to permit all the requests
+		 
+
+	/*	http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
+		 .httpBasic();
+		
+
+	} */
+
+	  @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		  auth.inMemoryAuthentication().withUser("test").password("test@1234").authorities("admin").and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+		  
+	}
+
+
+
+}
---
+++ src/main/java/com/eazybytes/springsecsection2/Springsecsection2Application.java
@@ -0,0 +1,18 @@
+package com.eazybytes.springsecsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@SpringBootApplication
+@ComponentScans({ @ComponentScan("com.eazybytes.controller"), @ComponentScan("com.eazybytes.config") })
+public class Springsecsection2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecsection2Application.class, args);
+	  
+	}
+
+}
